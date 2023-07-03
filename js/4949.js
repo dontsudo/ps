378f558c659aedc17e8d954c02584903689109e6
superposition82 <chotnt741@gmail.com>
2023-07-03 22:16:03 +0900

    4949 stack

---
+++ js/4949.js
@@ -0,0 +1,37 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const BALANCE_TABLE = {
+  '(': ')',
+  '[': ']'
+};
+
+function isBalancedString(str) {
+  const stack = [];
+  for (const char of str) {
+    if (char in BALANCE_TABLE) {
+      stack.push(char);
+    } else if (
+      stack.length === 0 ||
+      BALANCE_TABLE[stack.pop()] !== char
+    ) {
+      return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+rl
+  .on('line', function (line) {
+    if (line === '.') {
+      rl.close();
+      return;
+    }
+    console.log(isBalancedString(line) ? 'yes' : 'no');
+  })
+  .on('close', () => {
+    process.exit(0);
+  });
--- js/9012.js
+++ js/9012.js
@@ -1,44 +1,39 @@
-const readline = require('readline')
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
 const input = [];
 
-rl
-  .on('line', (line) => {
-    input.push(line);
-  })
-  .on('close', () => {
-    const [_, ...arr] = input;
+rl.on("line", (line) => {
+  input.push(line);
+}).on("close", () => {
+  const [_, ...arr] = input;
 
-    for (const str of arr) {
-      if (isVPS(str)) console.log("YES");
-      else console.log("NO");
-    }
-  })
+  for (const str of arr) {
+    if (isVPS(str)) console.log("YES");
+    else console.log("NO");
+  }
+});
 
 const BRACKET_MAP = {
-  '(' : ')',
-  '[' : ']',
-  '{' : '}'
-}
-    
+  "(": ")",
+  "[": "]",
+  "{": "}",
+};
+
 function isVPS(str) {
   const stack = [];
   for (const c of str) {
     if (c in BRACKET_MAP) {
       stack.push(c);
-    } else {
-      if (stack.length === 0) 
-        return false;
-      if (BRACKET_MAP[stack.pop()] !== c) {
-        return false;
-      }
+    } else if (
+      stack.length === 0 ||
+      BRACKET_MAP[stack.pop()] !== c
+    ) {
+      return false;
     }
   }
   return stack.length === 0;
 }
-
-
